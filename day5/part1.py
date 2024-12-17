3e76faf46aee9ba43a79470cd811de91375dd93d
louisejooones <louisejones94@live.co.uk>
2024-12-17 19:52:58 +0000

    Part 1

--- day5/part1.py
+++ day5/part1.py
@@ -5,37 +5,45 @@ input_data = get_data(day=5, year=2024)
 
 lines = [line for line in input_data.strip().split('\n')]
 
-rules = sorted(lines[:lines.index('')], key=lambda x: x.split('|')[0])
-rules = [rule.split('|') for rule in rules]
+rules = sorted(lines[:lines.index('')])
+rules_df = pd.DataFrame(rules, columns=['rule'])
 
 updates = lines[lines.index('')+1:]
-updates = [update.split(',') for update in updates]
+updates_df = pd.DataFrame(updates, columns=['update'])
 
-def check_rule(num1, num2, rule):
-    return num1 == rule[0] and num2 == rule[1]
+def check_rule(nums, rule):
+    rule_list = rule.split('|')
+    return nums.index(rule_list[0]) < nums.index(rule_list[1])
 
-def check_left(num, item, rules):
-    return sum([check_rule(item, num, rule) for rule in rules])
+def check_rules(nums, rules):
+    return all([check_rule(nums, rule) for rule in rules])
 
-def check_right(num, item, rules):
-    return sum([check_rule(num, item, rule) for rule in rules])
+def find_relevant_rule_for_num_pair(num1, num2):
+    relevant_rule = rules_df[(rules_df['rule'] == f'{num1}|{num2}') | (rules_df['rule'] == f'{num2}|{num1}')]
+    return relevant_rule['rule'].to_string(index=False).strip()
 
-for update in updates:
-    for i, item in enumerate(update):
-        left_elements = update[:i]
-        right_elements = update[i+1:]
-        for element in left_elements:
-            left_check_result = check_left(element, item, rules)
-        for element in right_elements:
-            right_check_result = check_right(element, item, rules)
+def find_relevant_rules_for_list_of_nums(nums):
+    relevant_rules = []
+    for i, num in enumerate(nums):
+        for j in range(i+1, len(nums)):
+            relevant_rules.append(find_relevant_rule_for_num_pair(num, nums[j]))
+    return relevant_rules
 
-full_order = []
+def find_middle_num(nums):
+    mid = len(nums) // 2
+    if len(nums) % 2 == 0:
+        return int(nums[mid-1:mid+1])
+    else:
+        return int(nums[mid])
 
-unique_numbers = set(rules[0])
+updates_df['update_list'] = updates_df['update'].apply(lambda x: x.split(','))
 
-full_order.append(rules[0][0])
-full_order.append(rules[0][1])
+updates_df['relevant_rules'] = updates_df.apply(lambda x: find_relevant_rules_for_list_of_nums(x['update_list']), axis=1)
 
-for num in unique_numbers:
-    if num not in full_order:
-        
+updates_df['check_rules'] = updates_df.apply(lambda x: check_rules(x['update_list'], x['relevant_rules']), axis=1)
+
+updates_df['middle_number'] = updates_df['update_list'].apply(lambda x: find_middle_num(x))
+
+result = updates_df[updates_df['check_rules']]['middle_number'].sum()
+
+print(result)
