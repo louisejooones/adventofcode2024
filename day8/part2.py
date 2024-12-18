ba34b716e395acb46dfd1ba8ee1895a68713d4cc
louisejooones <louisejones94@live.co.uk>
2024-12-18 20:10:02 +0000

    Part 2

--- day8/part2.py
+++ day8/part2.py
@@ -3,18 +3,6 @@ import numpy as np
 from aocd import get_data
 
 input_data = get_data(day=8, year=2024)
-input_data = """............
-........0...
-.....0......
-.......0....
-....0.......
-......A.....
-............
-............
-........A...
-.........A..
-............
-............"""
 
 lines = [line for line in input_data.strip().split('\n')]
 
@@ -32,16 +20,29 @@ def find_antinodes(coords):
 
     for i in range(len(coords) - 1):
         for j in range(i + 1, len(coords)):
+            print(coords[i], coords[j])
+            x_len = grid.shape[0]
+            y_len = grid.shape[1]
+            
             potential_antinodes = []
 
             x_diff = coords[i][0] - coords[j][0]
             y_diff = coords[i][1] - coords[j][1]
 
-            
 
+            max_diffs = max(x_len, y_len)+1
+
+            for diff in range(-max_diffs, max_diffs + 1):
+                if x_diff != 0 and y_diff != 0:
+                    potential_antinodes.append((coords[i][0] + x_diff * diff, coords[i][1] + y_diff * diff))
+                elif x_diff == 0:
+                    potential_antinodes.append((coords[i][0], coords[i][1] + y_diff * diff))
+                elif y_diff == 0:
+                    potential_antinodes.append((coords[i][0] + x_diff * diff, coords[i][1]))
+            
             for antinode in potential_antinodes:
-                if 0 <= antinode[0] < grid.shape[0] and 0 <= antinode[1] < grid.shape[1]:
-                    antinodes.append(antinode)  
+                if 0 <= antinode[0] < x_len and 0 <= antinode[1] < y_len:
+                    antinodes.append(antinode)
     return antinodes
 
 locs['antinodes'] = locs['coordinates'].apply(find_antinodes)
